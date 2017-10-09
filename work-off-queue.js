c25f82a4a0459db84abf3583f9fb0fd3b8d7d7b4
Dominik Kundel <dominik.kundel@gmail.com>
2017-10-09 13:37:17 +0200

    :sparkles: Factor out host url into env variable

--- work-off-queue.js
+++ work-off-queue.js
@@ -1,7 +1,7 @@
 const twilio = require('twilio');
 const client = twilio();
 
-const serverUrl = 'http://dk.ngrok.io/work-off-queue';
+const serverUrl = process.env.HOST_URL || 'http://dk.ngrok.io';
 const twilioNumber = process.argv[2] || process.env.TWILIO_PHONE_NUMBER;
 const targetPhoneNumber = process.argv[3] || process.env.MY_PHONE_NUMBER;
 
@@ -9,7 +9,7 @@ client.calls
   .create({
     from: twilioNumber,
     to: targetPhoneNumber,
-    url: serverUrl
+    url: `${serverUrl}/work-off-queue`
   })
   .then(() => {
     console.log('Your phone should be ringing!');
