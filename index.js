bc63e8c50c89dc647ba9e11c0351cf01036560ce
Dominik Kundel <dominik.kundel@gmail.com>
2017-10-09 13:19:17 +0200

    :sparkles: Add initial server

---
+++ index.js
@@ -0,0 +1,39 @@
+const { VoiceResponse } = require('twilio').twiml;
+const express = require('express');
+
+const app = express();
+
+const QUEUE_NAME = 'myqueue';
+const CONFERENCE_NAME = 'myconference';
+const PORT = process.env.PORT || 3000;
+
+app.post('/call', (req, res) => {
+  const twiml = new VoiceResponse();
+  twiml.say(
+    { voice: 'alice' },
+    `Thanks for calling the ultimate call center. You'll be connected shortly!`
+  );
+  twiml.enqueue({ waitUrl: '/wait' }, 'myqueue');
+  res.type('text/xml').send(twiml.toString());
+});
+
+app.post('/wait', (req, res) => {
+  const twiml = new VoiceResponse();
+  twiml.say(
+    { voice: 'alice' },
+    'While you are waiting, feel free to chat with other people waitin!'
+  );
+  twiml.dial().conference('myconference');
+  res.type('text/xml').send(twiml.toString());
+});
+
+app.post('/work-off-queue', (req, res) => {
+  const twiml = new VoiceResponse();
+  twiml.say({ voice: 'alice' }, 'Connecting to next person in the queue.');
+  twiml.dial().queue('myqueue');
+  res.type('text/xml').send(twiml.toString());
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
