7bdfe60a0613aa89be410720a778517b11b32bff
Dominik Kundel <dominik.kundel@gmail.com>
2017-10-09 15:47:31 +0200

    :sparkles: Change argument order of phone numbers

--- work-off-queue.js
+++ work-off-queue.js
@@ -2,8 +2,8 @@ const twilio = require('twilio');
 const client = twilio();
 
 const serverUrl = process.env.HOST_URL || 'http://dk.ngrok.io';
-const twilioNumber = process.argv[2] || process.env.TWILIO_PHONE_NUMBER;
-const targetPhoneNumber = process.argv[3] || process.env.MY_PHONE_NUMBER;
+const targetPhoneNumber = process.argv[2] || process.env.MY_PHONE_NUMBER;
+const twilioNumber = process.argv[3] || process.env.TWILIO_PHONE_NUMBER;
 
 client.calls
   .create({
